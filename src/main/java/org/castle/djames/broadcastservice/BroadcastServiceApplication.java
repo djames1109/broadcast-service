ba8a429704b8c578c872f71aed522d62deb6e4dd
DJames <davejames1109@gmail.com>
2023-01-02 19:50:02 +0800

    Initial Commit
    (cherry picked from commit 28f0876e4265be263bfe525081f2c241450d9648)

---
+++ src/main/java/org/castle/djames/broadcastservice/BroadcastServiceApplication.java
@@ -0,0 +1,13 @@
+package org.castle.djames.broadcastservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroadcastServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroadcastServiceApplication.class, args);
+    }
+
+}
